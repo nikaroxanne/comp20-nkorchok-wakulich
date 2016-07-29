4e18bdfec9bd08150697a12a40359b91b2fe7495
Nika Korchok Wakulich <nika.korchok_wakulich@tufts.edu>
2016-07-28 21:37:22 -0400

    Edited Lab 6 and 8

---
+++ mbta/mbta.js
@@ -0,0 +1,13 @@
+<script src="http://maps.googleapis.com/maps/api/js"></script>
+<script type=text/javascript></script>
+function init() {
+    var mapProp = {
+        center:new.google.maps.LatLng()
+        zoom: 7;
+        mapTypeId:google.maps.MapTypeId.HYBRID
+    }
+}
+
+var Lat = 0;
+var Lng = 0;
+
