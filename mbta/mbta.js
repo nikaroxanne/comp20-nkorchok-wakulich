e2aaecfc112fa9c15deeac097abbd8c2baaa6c3e
Nika Korchok Wakulich <nika.korchok_wakulich@tufts.edu>
2016-08-05 16:42:25 -0400

    Cleaned up code by combining operations and concatenating arrays of information

--- mbta/mbta.js
+++ mbta/mbta.js
@@ -1,30 +1,14 @@
-var userLat = 0;
-var userLng = 0;
-var request = new XMLHttpRequest();
-var user = new google.maps.LatLng(userLat, userLng);
+//var userLat = 42.406510;
+//var userLng = -71.119352;
+//var request = new XMLHttpRequest();
+//var user = new google.maps.LatLng(userLat, userLng);
 var map;
 var marker;
-var infowindow = new google.maps.InfoWindow({map:map});
+var infoWindow = new google.maps.InfoWindow();
 
 
 var redLineCenter = new google.maps.LatLng(42.352271, -71.05524200000001);
-/*
-var redLineAlewifeToJFK = [
-        {lat: 42.395428, lng: -71.142483},
-        {lat:42.39674, lng:-71.121815},
-        {lat:42.3884,  lng:-71.11914899999999},
-        {lat: 42.373362, lng:-71.118956},
-        {lat:42.365486, lng:-71.103802},
-        {lat:42.395428, lng:-71.142483},
-        {lat:42.361166, lng:-71.070628},
-        {lat:42.35639457, lng:-71.0624242},
-        {lat:42.355518, lng:-71.060225},
-        {lat:42.352271, lng:-71.05524200000001},
-        {lat:42.342622, lng: -71.056967},
-        {lat:42.330154, lng:-71.057655},
-        {lat:42.320685, lng:-71.052391}
-    ];
-*/
+
 var redLineAlewifeToJFK = [
         {stationName: 'Alewife', lat: 42.395428, lng: -71.142483},
         {stationName:'Davis', lat:42.39674, lng:-71.121815},
@@ -81,7 +65,7 @@ function init(){
         title: "South Station"
     });
     redCenterMarker.setMap(map);
-/*
+
     for(var i =0; i<redLineStations.length; i++){
         var position = new google.maps.LatLng(redLineStations[i].lat, redLineStations[i].lng);
         var title = redLineStations[i].stationName;
@@ -91,8 +75,10 @@ function init(){
                 icon: image,
                 title: title
         });
+    }
     //sets markers from Alewife to JFK
-*/
+
+    /*
     for(var i =0; i<redLineAlewifeToJFK.length; i++){
         var position = new google.maps.LatLng(redLineAlewifeToJFK[i].lat, redLineAlewifeToJFK[i].lng);
         var title = redLineAlewifeToJFK[i].stationName;
@@ -103,27 +89,7 @@ function init(){
                 title: title
         });
     }
-    //sets polypath from Alewife to JFK
-    /*
-     var redPolyLinePath1 = [
-        new google.maps.LatLng(redLineAlewifeToJFK[0].lat,redLineAlewifeToJFK[0].lng),
-        new google.maps.LatLng(redLineAlewifeToJFK[1].lat,redLineAlewifeToJFK[1].lng),
-        new google.maps.LatLng(redLineAlewifeToJFK[2].lat,redLineAlewifeToJFK[2].lng),
-        new google.maps.LatLng(redLineAlewifeToJFK[3].lat,redLineAlewifeToJFK[3].lng),
-        new google.maps.LatLng(redLineAlewifeToJFK[4].lat,redLineAlewifeToJFK[4].lng),
-        new google.maps.LatLng(redLineAlewifeToJFK[5].lat,redLineAlewifeToJFK[5].lng),
-        new google.maps.LatLng(redLineAlewifeToJFK[6].lat,redLineAlewifeToJFK[6].lng),
-        new google.maps.LatLng(redLineAlewifeToJFK[7].lat,redLineAlewifeToJFK[7].lng),
-        new google.maps.LatLng(redLineAlewifeToJFK[8].lat,redLineAlewifeToJFK[8].lng),
-        new google.maps.LatLng(redLineAlewifeToJFK[9].lat,redLineAlewifeToJFK[9].lng),
-        new google.maps.LatLng(redLineAlewifeToJFK[10].lat,redLineAlewifeToJFK[10].lng),
-        new google.maps.LatLng(redLineAlewifeToJFK[11].lat,redLineAlewifeToJFK[11].lng)
-    ];
-    */
-
-        //for (var j=0; j<redLineAlewifeToJFK.length; j++){
-      //      var redPolyLinePath1 = new google.maps.LatLng(redLineAlewifeToJFK[j].lat,redLineAlewifeToJFK[j].lng);}
-
+*/
     var redPolyline1 = new google.maps.Polyline({
             path:redLineAlewifeToJFK,
             //geodesic: true,
@@ -132,7 +98,7 @@ function init(){
             strokeWeight: 2,
             });
     redPolyline1.setMap(map);
-    
+ /*   
     //sets markers from JFK to Braintree
     for(var j =0; j<redLineJFKToBraintree.length; j++){
         var position = new google.maps.LatLng(redLineJFKToBraintree[j].lat, redLineJFKToBraintree[j].lng);
@@ -144,6 +110,8 @@ function init(){
                 title: title
         });
     }
+
+  */  
     //sets Polyline from JFK to Braintree
     var redPolyline2 = new google.maps.Polyline({
         path:redLineJFKToBraintree,
@@ -153,7 +121,7 @@ function init(){
         strokeWeight: 2,
     });
     redPolyline2.setMap(map);
-
+/*
     //sets markers from JFK to Ashmont
     for(var k =0; k<redLineJFKToAshmont.length; k++){
         var position = new google.maps.LatLng(redLineJFKToAshmont[k].lat, redLineJFKToAshmont[k].lng);
@@ -166,6 +134,8 @@ function init(){
                 title: title
         });
     }
+
+*/
     //sets Polyline from JFK to Ashmont
     var redPolyline3 = new google.maps.Polyline({
         path:redLineJFKToAshmont,
@@ -175,44 +145,102 @@ function init(){
         strokeWeight: 2,
     });
     redPolyline3.setMap(map);
-    findMe();
+    //findMe();
+    //showMe();
 }
+/*
+var image2 = "whereAreYou.png";
+function showMe(){
+    var userLat = 42.406510;
+    var userLng = -71.119352;
+    var position = new google.maps.LatLng(userLat, userLng);
+    //map.panTo(position);
+    var marker = new google.maps.Marker({
+        position: position,
+        title: 'Here you are',
+        icon: image2
+    });
+    marker.setMap(map);
 
+    google.maps.event.addListener(marker, 'click', function(){
+        infoWindow.setContent(marker.title);
+        infoWindow.open(map,marker);
+    });
+}
+*/
+
+/*
 request = new XMLHttpRequest();
-request.open("GET", " https://powerful-depths-66091.herokuapp.com/redline.json");
+request.open("GET", "https://powerful-depths-66091.herokuapp.com/redline.json",true);
 request.onreadystatechange = schedule;
 request.send(null);
 function schedule(){
     if(request.readyState == 4 && request.status == 200){
         result = "";
-        raw = request.responseText;
-        redLineData = JSON.parse(raw);
+        //raw = request.responseText;
+        redLineData = JSON.parse(request.responseText);
        // elem = getElementById("list");
         for (i=0; i < redLineData["TripList"]["Trips"].length; i++){
-            result += "The Next Scheduled Train to" + " " + ["TripList"]["Trips"][i]["Predictions"][i]["Stops"] + ", " + redLineData["TripList"]["Trips"][i]["Destination"] +  "will arrive in " + redLineData["TripList"]["Trips"][i]["Predictions"][i]["Seconds"] + " seconds</p>";
+            result += "<p>The Next Scheduled Train to" + " " + ["TripList"]["Trips"][i]["Predictions"][0]["Stop"] + ", " + redLineData["TripList"]["Trips"][i]["Destination"] +  " bound, will arrive in " + redLineData["TripList"]["Trips"][i]["Predictions"][0]["Seconds"] + " seconds</p>";
         }
-    } else if (request.readyState == 4 && request.status ==200) {
+        document.getElementById("list").innerHTML = result;
+    } else if (request.readyState == 4 && request.status != 200) {
         document.getElementById("list").innerHTML = "<p> Oh no, your browser doesn't support this feature. </p> "
     }
 };
+*/
+/*
 
-function findMe(){
-    if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(function(position) {
-                userLat = position.coords.latitude,
-                userLng = position.coords.longitude,
-                renderMap();
-                closestDistance();
-            });
+if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        };
+
+        infoWindow.setPosition(pos);
+        infoWindow.setContent('You are here.');
+        //map.setCenter(pos);
+    }, function () {
+        handleLocationError(true, infoWindow, map.getCenter());
+    });
+
+} else {
+   handleLocationError(false, infoWindow, map.getCenter());
+}
+
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos){
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+            'Error: The Geolocation service failed.' :
+            'Error: Your browser doesn\'t support geolocation.');
+}
+  */ 
+    /*
     } else {
         alert("Geolocation service did not work." + "<br>" + "Please check your browser and verify that you are using one that supports geolocation.");
     }
 }
-
+*/
+/*
+function findMe() {
+    if (navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(function(position){
+            userLat = position.coords.latitude;
+            userLng = position.coords.longitude;
+            renderMap();
+            });
+    } else {
+        alert("Your browser doesn't support Geolocation. Sorry.");
+    }
+}
+*/
+/*
 var markerImage = "whereAreYou.png";
 function renderMap(){
     user = new google.maps.LatLng(userLat, userLng);
-    //map.panTo(user);
+    map.panTo(user);
     marker = new google.maps.Marker({
         position: user,
         title: 'You are here!',
@@ -225,7 +253,8 @@ function renderMap(){
         infowindow.open(map, marker)
     });
 }
-
+*/
+/*
 function closestDistance(userLat,usrLng){
     var pi = Math.pi;
     var R = 6371;
@@ -249,5 +278,5 @@ function closestDistance(userLat,usrLng){
     }
     alert(redLineStations[closest]);
 }
-
+*/
 //google.maps.event.addDomListener(window,'load',initialize);
